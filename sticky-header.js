f9f51b78c3ce9851521cec32e4a4d95e444f49a2
Colton Childers <coltonchilders@gmail.com>
2017-09-19 10:25:57 -0500

    First commit
    all initial work up until now

---
+++ sticky-header.js
@@ -0,0 +1,13 @@
+$(function(){
+  $(window).scroll(function(){
+    var winTop = window.scrollY;
+    if(winTop >= 700){
+      $('.header').addClass('shrink').addClass('navbar-fixed-top');
+        $('.offset').addClass('shrink');
+    }
+    else if (winTop < 700) {
+      $('.header').removeClass('shrink').removeClass('navbar-fixed-top');
+        $('.offset').removeClass('shrink');
+    }//if-else
+  });//win func.
+});//ready func.
