1139dfa0f03f7cc0c7759b786c3c3b2c6fd2db7e
Colton Childers <coltonchilders@gmail.com>
2017-09-21 15:18:27 -0500

    added social media footer

--- sticky-header.js
+++ sticky-header.js
@@ -2,11 +2,11 @@ $(function(){
   $(window).scroll(function(){
     var winTop = window.scrollY;
     if(winTop >= 800){
-      $('.header').addClass('shrink').addClass('navbar-fixed-top');
+      $('header').addClass('shrink').addClass('navbar-fixed-top');
         $('.offset').addClass('shrink');
     }
     else if (winTop < 800) {
-      $('.header').removeClass('shrink').removeClass('navbar-fixed-top');
+      $('header').removeClass('shrink').removeClass('navbar-fixed-top');
         $('.offset').removeClass('shrink');
     }//if-else
   });//win func.
